459b0838c0c4afa1a05bc08dea1678199b6136ff
Benjamim Pinheiro <76885866+numdez@users.noreply.github.com>
2024-10-15 19:29:07 -0300

    removidos imports não utilizados

--- app/controller/rotas.py
+++ app/controller/rotas.py
@@ -6,11 +6,6 @@ from app.model.forms import LoginForm, AddAtendimentoForm
 from app.model.entidades.Usuario import Usuario
 from app.log.logger import log_action
 
-from cryptography import x509
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import serialization
-import os
-
 import base64
 from io import BytesIO
 from PIL import Image
